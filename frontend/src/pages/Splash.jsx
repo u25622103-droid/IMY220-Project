360c11b109fc6f16211ee3ae776f008908cce0c1
u25622103-droid <u25622103@tuks.co.za>
2026-09-02 20:35:24 +0200

    Styled splash

--- frontend/src/pages/Splash.jsx
+++ frontend/src/pages/Splash.jsx
@@ -1,8 +1,25 @@
+import { Link } from "react-router";
 import "../css/Splash.css";
 
 function Splash() {
     return(
-        <h1>Splash</h1>
+        <>
+            <div className="splash-bg"></div>
+            <div className="splash-main">
+                <div className="splash-description">
+                    <p>BEAUTIFY is a photo sharing platform where photographers, artists, and dreamers come together to showcase 
+                    their unique perspectives.</p>
+                    <p> Whether you're a professional photographer or just love capturing everyday moments, </p>
+                    <p>BEAUTIFY is your canvas, Share Your Vision.</p>
+                    <p>Curate your best work into beautiful collections</p>
+                    <p>Connect with fellow photographers and grow together</p>
+                </div>
+                <div className="splash-buttons">
+                    <Link to="/Login">Login</Link>
+                    <Link to="/SignUp">Sign-up</Link>
+                </div>
+            </div>
+        </>
     );
 }
 
