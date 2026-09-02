b9c34004e629dfc78062f28a3c9f7aafe0ef33de
u25622103-droid <u25622103@tuks.co.za>
2026-09-02 20:38:18 +0200

    Fixed routing for login and signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,41 +3,41 @@ import Navbar from './components/Navbar';
 import Splash from "./pages/Splash"
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import Posts from './pages/Posts';
+import Posts from './pages/Pages';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import "./css/App.css";
 
 function App() {
   const location = useLocation();
-  const isSplashPage = location.pathname === '/';
+  // Pages that should NOT show the navbar/heading
+  const noLayoutPages = ['/', '/login', '/signup'];
+  const showLayout = !noLayoutPages.includes(location.pathname);
 
   return (
     <div className="app-wrapper">
-      {/* Only show heading and navbar if NOT on splash page */}
-      {!isSplashPage && (
+      {showLayout ? (
         <>
           <h1>BEAUTIFY</h1>
           <div className='main'>
             <Navbar />
             <div className='content'>
               <Routes>
-                <Route path="/" element={<Splash />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/posts" element={<Posts />} />
+                {/* Redirect any unknown routes to home */}
+                <Route path="*" element={<Home />} />
               </Routes>
             </div>
           </div>
         </>
-      )}
-      
-      {/* Show splash page without heading and navbar */}
-      {isSplashPage && (
+      ) : (
+        // Show splash/login/signup without heading and navbar
         <Routes>
           <Route path="/" element={<Splash />} />
-          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       )}
     </div>
